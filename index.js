591bc291474e1accbdfc61f5ddfe46680c0dab36
Mollie <molliesmith22@gmail.com>
2018-10-25 18:42:54 -0500

    practice to do list

--- index.js
+++ index.js
@@ -5,6 +5,37 @@
 // >    - Remove from List
 // >    - Display List
 
-const args = process.argv;
+// program
+//   .version('0.1.0')
+//   .option('-p, --peppers', 'Add peppers')
+//   .option('-P, --pineapple', 'Add pineapple')
+//   .option('-b, --bbq-sauce', 'Add bbq sauce')
+//   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+//   .parse(process.argv);
 
-console.log(args);
+const fs = require('fs');
+
+const program = require('commander');
+
+program
+    .version ('1.0')
+    .option('-p, --print', 'Print Menu')
+    .option('-a, --add [name]', 'Add to List')
+    .option('-r, --remove [name]', 'Remove from List')
+    .option('-d, --display', 'Display List')
+    .parse(process.argv);
+
+if(program.print) {
+    console.log('Print Menu');
+}
+
+if(program.display) {
+    console.log('Display List')
+}
+
+function addItemToList(itemName) {
+    console.log('Added', itemName)
+    fs.writeFileSync('./list.txt' , [itemName])
+}
+
+fs.writeFileSync('./testfile.json', 'Hello Everybody!');
