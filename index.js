a28ba0bb9abc42fb34937a638a938ae0591b8567
Mollie <molliesmith22@gmail.com>
2018-10-25 17:44:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Homework from OKCoders
+// Create a command line to do list using Commander
+// >  - Print menu
+// >    - Add to List
+// >    - Remove from List
+// >    - Display List
+
+const args = process.argv;
+
+console.log(args);
